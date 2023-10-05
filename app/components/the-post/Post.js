5545064a0a9b729d1b454ba511d7e5969509a64e
melissastav <melissadodwell@gmail.com>
2023-10-05 15:18:35 +0100

    post

--- app/components/the-post/Post.js
+++ app/components/the-post/Post.js
@@ -4,10 +4,11 @@ export default function Post
 
 ({name, title, content, image, likes}) {
 
-<div className="bg-red-500 h-screen w-screen">
-    <h2 className="">{name}</h2>
-    <img className="" src={image}/>
-    <h3 className="">{title}</h3>
+
+<div className="bg-blue-300 h-screen w-screen flex flex-col justify-center">
+    <h2 className="m-3 font-bold">{name}</h2>
+    <img className="m-3 rounded-2xl h-10 w-10" src={image}/>
+    <h3 className="font-">{title}</h3>
     <p className="">{content}</p>
     <p className="">{likes}</p>
 </div>
