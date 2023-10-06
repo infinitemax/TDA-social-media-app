f782e953ea8b4968ef18a909c7fafb672ba2f0fe
infinitemax <m.brookman.byrne@gmail.com>
2023-10-06 17:03:37 +0100

    style home page

--- app/page.js
+++ app/page.js
@@ -5,23 +5,10 @@ import TestPost from "./components/max-test-components/TestPost";
 
 export default function Home() {
   return (
-    <main>
-      <div className="h-screen w-screen">
-        <h1 className="pt-24">hello</h1>
-        <Post
-          name="user"
-          image="image"
-          content="hello"
-          title="title"
-          likes="0"
-        />
-
-        <TestPost 
-          title="test"
-          name="test"
-          content="test test test"
-          likes="0"
-        />
+    <main className="p-2 min-h-screen w-screen bg-blue-300 pt-20 sm:pt-20 md:pt-10">
+      <div className="h-screen w-screen flex items-center flex-col">
+        <h1 className="text-4xl mt-12">Welcome to the best social media app in town!</h1>
+        <h2 className="mt-12 text-2xl">Click <a href="/profile">"profile"</a> so see your posts....</h2>
 
 
       </div>
